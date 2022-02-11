49c16c316eceeaf10d5e449759a287af288491c8
Suraj Pandey <suraj.pandey@growisto.com>
2022-02-11 23:32:07 +0530

    funygym

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// let,var,cont 
+
+
+// function varTest(){
+//     var x =2;
+//  console.log("x ", x);
+//     {
+//         var x =3;
+//     console.log("x ", x);
+        
+//     }
+//  console.log("x ", x);
+    
+// }
+// varTest();
+
+
+function letTest(){
+    var y =2;
+    {
+        let y =3;
+    console.log("y ", y);
+        
+    }
+ console.log("y ", y);
+    
+}
+letTest();
